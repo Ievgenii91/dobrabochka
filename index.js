1a78653ba182344926211b3e6b78e1b65ca18be1
ievgenii <iebutyrin@deloitte.com>
2020-07-16 15:05:46 +0300

    worker

--- index.js
+++ index.js
@@ -8,14 +8,13 @@ nconf.argv()
 const runBot = require('./bot')
 const runSPDBot = require('./spd_bot')
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+// express()
+//   .use(express.static(path.join(__dirname, 'public')))
+//   .set('views', path.join(__dirname, 'views'))
+//   .set('view engine', 'ejs')
+//   .get('/', (req, res) => res.render('pages/index'))
+//   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
 // BOT for sms
 runBot();
-
 runSPDBot();
