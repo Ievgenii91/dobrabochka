9e36577d3f41c7ec1132ed4fe12a9810187dd842
Ievgenii Butyrin <iebutyrin@deloitte.com>
2020-10-16 10:50:07 +0300

    123

--- index.js
+++ index.js
@@ -12,7 +12,11 @@ express()
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
   .get('/woo', (req, res) => {
-    console.log('test2', req.params, req.query)
+    console.log('test2 GET', req.params, req.query)
     return res.status(200).send({ ok: true })
   })
+  .post('/woo', (req, res) => {
+    console.log('test2 POST', req.body)
+    return res.status(200).send({ ok: true, post: true })
+  })  
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
