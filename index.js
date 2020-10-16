67de48cb0d0e0f383a3a822fce726fba8d9acfe6
Ievgenii Butyrin <iebutyrin@deloitte.com>
2020-10-16 11:05:33 +0300

    4

--- index.js
+++ index.js
@@ -2,11 +2,14 @@ const express = require('express')
 const path = require('path')
 const nconf = require('nconf');
 const PORT = process.env.PORT || 5000
+const bodyParser = require('body-parser')
 nconf.argv()
     .env()
     .file({ file: 'config.json' });
 
 express()
+  .use(bodyParser.json())
+  .use(bodyParser.urlencoded({ extended: false }))
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
